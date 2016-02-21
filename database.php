77d5ea646e6afc68195652dedc9730ae61061a31
bitcrystal <bitcrystalv30@gmail.com>
2016-02-21 07:27:16 +0100

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 $dbdb_host = 'localhost';         // the server the database is located
 $dbdb_user = 'bitcoinexchange';      // the username to access the database
-$dbdb_pass = 'fickdiekackwurst';      // the password to access the database
+$dbdb_pass = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';      // the password to access the database
 $dbdb_database = 'bitcoinexchange';  // the name of the database being used
 ?>
